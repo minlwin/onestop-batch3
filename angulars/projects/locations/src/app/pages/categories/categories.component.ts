a0e076d1f96f5ab0cefdf5c3d1d1c40b56bb8f65
Min Lwin <lwin.zawmin@gmail.com>
2023-03-08 14:05:02 +0630

    Adding HTTP Client

--- angulars/projects/locations/src/app/pages/categories/categories.component.ts
+++ angulars/projects/locations/src/app/pages/categories/categories.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../apis/category.service';
+
+declare const bootstrap:any
 
 @Component({
   templateUrl: './categories.component.html',
   styles: [
   ]
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit, AfterViewInit{
 
+  list:any[] = []
+
+  private modal:any
+
+  constructor(private service:CategoryService) {}
+
+  ngOnInit(): void {
+    this.service.search().subscribe(result => this.list = result)
+  }
+
+  ngAfterViewInit(): void {
+    this.modal = new bootstrap.Modal('#categoryEditModal', {})
+  }
+
+  addNew() {
+    this.modal.show()
+  }
+
+  edit(data:any) {
+
+  }
 }
