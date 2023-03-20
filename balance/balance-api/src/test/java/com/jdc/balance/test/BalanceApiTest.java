3c07cc8a99a3e40151f2b25f607782c149906653
Min Lwin <lwin.zawmin@gmail.com>
2023-03-20 19:56:40 +0630

    Adding Balance API Test Cases Stub

--- balance/balance-api/src/test/java/com/jdc/balance/test/BalanceApiTest.java
+++ balance/balance-api/src/test/java/com/jdc/balance/test/BalanceApiTest.java
@@ -1,53 +1,10 @@
 package com.jdc.balance.test;
 
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
 
-import com.jdc.balance.BalanceAppWebConfig;
-
-@TestMethodOrder(value = OrderAnnotation.class)
-@SpringJUnitWebConfig(classes = BalanceAppWebConfig.class)
+@Suite
+@SelectPackages("com.jdc.balance.test.balance")
 public class BalanceApiTest {
 	
-	@Order(1)
-	void test_search() {
-		
-	}
-
-	@Order(2)
-	void test_find_by_id_not_found() {
-		
-	}
-
-	@Order(3)
-	void test_find_by_id_success() {
-		
-	}
-	
-	@Order(4)
-	void test_create_validation_error() {
-		
-	}
-	
-	@Order(5)
-	void test_create_success() {
-		
-	}
-	
-	@Order(6)
-	void test_update_no_data() {
-		
-	}
-
-	@Order(7)
-	void test_update_validation_error() {
-		
-	}
-	
-	@Order(8)
-	void test_update_success() {
-		
-	}
 }
---
+++ balance/balance-api/src/test/java/com/jdc/balance/test/balance/BalanceApiSearchTest.java
@@ -0,0 +1,33 @@
+package com.jdc.balance.test.balance;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.jdc.balance.BalanceAppWebConfig;
+
+@WithMockUser(username = "test", authorities = "Member")
+@SpringJUnitWebConfig(classes = BalanceAppWebConfig.class)
+@Sql(scripts = {
+		"classpath:/sql/test_users.sql",
+		"classpath:/sql/test_ledgers.sql",
+})
+public class BalanceApiSearchTest {
+
+	WebTestClient client;
+	
+	@BeforeEach
+	void setUp(WebApplicationContext context) {
+		client = MockMvcWebTestClient.bindToApplicationContext(context)
+				.build();
+	}
+
+	void test_search() {
+		
+	}
+
+}
